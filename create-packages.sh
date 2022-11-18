2dfcb4478e2b6374d1edee1226210a98a6060adf
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-11-18 16:34:45 +0100

    Dont package repo files

--- create-packages.sh
+++ create-packages.sh
@@ -4,15 +4,14 @@
 apt-get download cgpt vboot-kernel-utils
 
 # Extract the contents of the packages
-dpkg-deb -x cgpt*.deb ./tar/
-dpkg-deb -x vboot-kernel-utils*.deb ./tar/
-
-# Delete deb packages
-rm ./*.deb
+dpkg-deb -x cgpt*.deb ./tar-package/
+dpkg-deb -x vboot-kernel-utils*.deb ./tar-package/
 
 # Edit arch package metadata
 sed -i "s/insert_date/$(date +%s)/" .PKGINFO # Insert current date
+cp .PKGINFO ./tar-package/.PKGINFO # Copy metadata to package
 
 # Compress to tar without . but with hidden files
-tar --zstd -cf ./vboot-cgpt-utils.pkg.tar.zst * .??*
+cd tar-package
+tar --zstd -cf ../vboot-cgpt-utils.pkg.tar.zst * .??*
 
